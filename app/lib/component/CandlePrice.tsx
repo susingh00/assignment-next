203c0d2fae1259be2203da922317975192092d95
susingh00 <susingh@deqode.com>
2023-06-15 12:03:57 +0530

    destructing

---
+++ app/lib/component/CandlePrice.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { series } from "../utils/constant";
+
+export function CandlePrice({
+  currentPrice,
+  label,
+}: {
+  currentPrice: number;
+  label: string;
+}) {
+  return (
+    <>
+      <span className="text-gray-400 mx-1">{label}</span>
+      <span className="text-green-600" id="price-action">
+        {currentPrice}
+      </span>
+    </>
+  );
+}
